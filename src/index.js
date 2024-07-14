7f9e0c4439dfd6a937e07f454fd608db10a405f6
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-07-14 13:28:34 -0500

    ajustes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ import {Server as SocketServer} from 'socket.io'
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer(app)
-const io = new SocketServer(server)
+const io = new SocketServer(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+})
+
+io.on('connection', socket => {
+    console.log('Cliente')
+    socket.on('message', (body) =>{
+        console.log(body)
+        socket.broadcast.emit('message', {
+            body,
+            from: socket.id.slice(2)
+        })
+    })
+})
 
 server.listen(PORT)
 console.log(`SERVER IS RUNNING ON POR ${PORT}`)
